145eee602c2b8ce685d5ddb914fedf0f52e2fb29
Z4pata <zapata.devs@gmail.com>
2024-08-05 10:39:27 -0500

    Feat: Se inicializo la interfaz y se agregó todo lo necesario para los perros

--- Program.cs
+++ Program.cs
@@ -1,2 +1,74 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PruebaC_sharp_JuanJoseZapata.Models;
+
+var veterinary = new VeterinaryClinic("Veterinaria", "Calle 51 #56A-49");
+int Id = 0;
+while (true)
+{
+    Console.Clear();
+    ManagerApp.ShowHeader("Clinica veterinaria");
+
+    string opc01 = VisualInterfaces.ShowMainMenu();
+
+    switch (opc01)
+    {
+        case "1":
+            bool dogFlag = true;
+            while (dogFlag)
+            {
+                string dogOpc = VisualInterfaces.ShowDogsMenu();
+
+                switch (dogOpc)
+                {
+                    case "1":
+                        Id += 1;
+                        veterinary.SaveDog(ManagerApp.CreateDog(Id));
+                        VisualInterfaces.ShowSaveSuccesful();
+                        ManagerApp.ShowFooter();
+                        break;
+                    case "2":
+                        int updateSearch = Settings.ValidateInt("Ingresa el Id del perrito que deseas actualizar: ");
+
+                        var updateDog = veterinary.Dogs.Find(d => d.PublicId == updateSearch);
+
+                        veterinary.UpdateDog(updateDog);
+
+                        ManagerApp.ShowFooter();
+                        break;
+                    case "3":
+                        int deleteSearch = Settings.ValidateInt("Ingresa el Id del perrito que deseas eliminar: ");
+
+                        veterinary.DeleteDog(deleteSearch);
+                        ManagerApp.ShowFooter();
+                        break;
+                    case "4":
+                        veterinary.ShowAnimals("perro");
+                        ManagerApp.ShowFooter();
+                        break;
+                    case "0":
+                        dogFlag = false;
+                        break;
+                    default:
+                        VisualInterfaces.ShowInputError();
+                        ManagerApp.ShowFooter();
+                        break;
+                }
+            }
+            break;
+
+        case "2":
+            break;
+
+        case "3":
+            break;
+
+        case "0":
+            return;
+
+        default:
+            VisualInterfaces.ShowInputError();
+            ManagerApp.ShowFooter();
+
+            break;
+
+    }
+}
