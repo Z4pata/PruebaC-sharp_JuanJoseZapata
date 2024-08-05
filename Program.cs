04bf9ca71940f5c054a11070bc879d7f06c80dcb
Z4pata <zapata.devs@gmail.com>
2024-08-05 12:42:50 -0500

    Feat: Se agrego una quinta opcion, se quemaron datos y se solucionaron unos errores de interfaz

--- Program.cs
+++ Program.cs
@@ -1,6 +1,22 @@
 ﻿using PruebaC_sharp_JuanJoseZapata.Models;
 
+// Se instancia la veterinaria ----------
+
 var veterinary = new VeterinaryClinic("Veterinaria", "Calle 51 #56A-49");
+
+// ------------------------------------------- QUEMO DATOS --------------------------------------------------------------
+
+veterinary.Dogs.Add(new Dog(0, "Juan", new DateOnly(2014, 9, 06), "pinscher", "verde", 12.5, true, "timido", "122349", "mucho", "pelo largo"));
+veterinary.Dogs.Add(new Dog(1, "Carlos", new DateOnly(2010, 12, 06), "Pinscher", "cafe", 8, true, "normal", "122349", "mucho", "pelo largo"));
+veterinary.Dogs.Add(new Dog(2, "Andres", new DateOnly(2014, 3, 06), "doberman", "negro", 30, true, "agresivo", "5675", "poco", "pelo largo"));
+
+
+veterinary.Cats.Add(new Cat(3, "Black", new DateOnly(2000, 5, 17), "egipcio", "rosado", 5, true, "pelo largo"));
+veterinary.Cats.Add(new Cat(4, "Blue", new DateOnly(2000, 6, 20), "otro", "cafe", 8, true, "pelo largo"));
+veterinary.Cats.Add(new Cat(5, "pelusa", new DateOnly(2000, 7, 8), "otro", "gris", 3, true, "pelo largo"));
+
+// ----------------------------------------------------------------------------------------------------------------------
+
 int Id = 0;
 while (true)
 {
@@ -15,6 +31,7 @@ while (true)
             bool dogFlag = true;
             while (dogFlag)
             {
+                Console.Clear();
                 string dogOpc = VisualInterfaces.ShowDogsMenu();
 
                 switch (dogOpc)
@@ -44,8 +61,51 @@ while (true)
                         veterinary.ShowAnimals("perro");
                         ManagerApp.ShowFooter();
                         break;
+
+                    case "5":
+                        int idSearch = Settings.ValidateInt("Ingrese el Id del perrito: ");
+
+                        bool pFlag = veterinary.ShowPatient(idSearch);
+                        ManagerApp.ShowFooter();
+
+                        while (pFlag)
+                        {
+                            
+                            Console.Clear();
+                            var patient = veterinary.Dogs.Find(d => d.PublicId == idSearch);
+
+                            string pOpc = VisualInterfaces.ShowPatientMenu("perro");
+
+                            switch (pOpc)
+                            {
+                                case "1":
+                                    patient.Hairdress();
+                                    ManagerApp.ShowFooter();
+                                    break;
+
+                                case "2":
+                                    patient.CastrateAnimal();
+                                    ManagerApp.ShowFooter();
+                                    break;
+
+                                case "0":
+                                    pFlag = false;
+                                    ManagerApp.ShowFooter();
+                                    break;
+
+                                default:
+                                    VisualInterfaces.ShowInputError();
+                                    ManagerApp.ShowFooter();
+                                    break;
+                            }
+
+
+                        }
+                        break;
+
                     case "0":
                         dogFlag = false;
+                        ManagerApp.ShowFooter();
                         break;
                     default:
                         VisualInterfaces.ShowInputError();
@@ -53,6 +113,7 @@ while (true)
                         break;
                 }
             }
+            
             break;
 
         case "2":
@@ -65,14 +126,14 @@ while (true)
                 switch (catOpc)
                 {
                     case "1":
-                        Id +=1;
+                        Id += 1;
                         veterinary.SaveCat(ManagerApp.CreateCat(Id));
                         VisualInterfaces.ShowSaveSuccesful();
                         ManagerApp.ShowFooter();
                         break;
-                    
+
                     case "2":
-                        int updateSearchCat= Settings.ValidateInt("Ingresa el Id del gatito que deseas actualizar: ");
+                        int updateSearchCat = Settings.ValidateInt("Ingresa el Id del gatito que deseas actualizar: ");
 
                         var updatedCat = veterinary.Cats.Find(c => c.PublicId == updateSearchCat);
 
@@ -80,7 +141,7 @@ while (true)
 
                         ManagerApp.ShowFooter();
                         break;
-                    
+
                     case "3":
                         int deleteSearchCat = Settings.ValidateInt("Ingresa el Id del gatito qur deseas eliminar: ");
 
@@ -88,16 +149,56 @@ while (true)
 
                         ManagerApp.ShowFooter();
                         break;
-                    
+
                     case "4":
                         veterinary.ShowAnimals("gato");
                         ManagerApp.ShowFooter();
                         break;
                     
+                    case "5":
+                    int idSearch = Settings.ValidateInt("Ingrese el Id del gatito: ");
+
+                        bool pFlag = veterinary.ShowPatient(idSearch);
+                        while (pFlag)
+                        {
+                            ManagerApp.ShowFooter();
+                            Console.Clear();
+                            var patient = veterinary.Cats.Find(d => d.PublicId == idSearch);
+
+                            string pOpc = VisualInterfaces.ShowPatientMenu("gato");
+
+                            switch (pOpc)
+                            {
+                                case "1":
+                                    patient.Hairdress();
+                                    ManagerApp.ShowFooter();
+                                    break;
+
+                                case "2":
+                                    patient.CastrateAnimal();
+                                    ManagerApp.ShowFooter();
+                                    break;
+
+                                case "0":
+                                    pFlag = false;
+                                    ManagerApp.ShowFooter();
+                                    break;
+
+                                default:
+                                    VisualInterfaces.ShowInputError();
+                                    ManagerApp.ShowFooter();
+                                    break;
+                            }
+
+
+                        }
+                        break;
+
                     case "0":
                         catFlag = false;
+                        ManagerApp.ShowFooter();
                         break;
-                    
+
                     default:
                         VisualInterfaces.ShowInputError();
                         ManagerApp.ShowFooter();
@@ -117,7 +218,7 @@ while (true)
                 switch (searchesOpc)
                 {
                     case "1":
-                    veterinary.ShowAllPatients();
+                        veterinary.ShowAllPatients();
                         ManagerApp.ShowFooter();
                         break;
 
@@ -132,16 +233,26 @@ while (true)
                         veterinary.ShowAnimals("perro");
                         ManagerApp.ShowFooter();
                         break;
-                    
+
                     case "4":
                         veterinary.ShowAnimals("gato");
                         ManagerApp.ShowFooter();
+                        break;
+                    
+                    case "0":
+                        searchesFlag = false;
+                        ManagerApp.ShowFooter();
+                        break;
+                    
+                    default:
+                        VisualInterfaces.ShowInputError();
                         break;
                 }
             }
             break;
 
         case "0":
+            ManagerApp.ShowFooter();
             return;
 
         default:
