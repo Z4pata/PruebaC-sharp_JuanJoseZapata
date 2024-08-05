86ff66bccf7ef5e673e69eb48fe234a65f19e2fe
Z4pata <zapata.devs@gmail.com>
2024-08-05 07:14:22 -0500

    Feat: Se Creo la clase Settings y un par de validaciones (string / int)

---
+++ Models/Settings.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PruebaC_sharp_JuanJoseZapata.Models
+{
+    public static class Settings
+    {
+        public static string ValidateString(string prompt)
+        {
+            string aValidar;
+
+            do
+            {
+                Console.Write(prompt);
+                aValidar = Console.ReadLine() ?? " "; // esto "??" me valida si es nulo, y si lo es me devuelve lo de la derecha
+
+            } while (string.IsNullOrWhiteSpace(aValidar));
+
+            return aValidar;
+        }
+        public static int ValidateInt(string prompt)
+        {
+            while (true)
+            {
+                Console.Write(prompt);
+                string sAvalidar = Console.ReadLine() ?? " ";
+
+                int result;
+                if (int.TryParse(sAvalidar, out result)){
+                    return result;
+                } else {
+                    VisualInterfaces.ShowNumberError();
+                }
+
+            }
+        }
+    }
+}
